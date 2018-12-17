d87c24a4e7a18d6d9e9cc3ed79af3fcda24e34f9
Anittttta <anita.gul90@gmail.com>
2018-12-17 13:18:40 +0200

    homework

--- src/main/java/lecture3/Solution1.java
+++ src/main/java/lecture3/Solution1.java
@@ -1,4 +1,70 @@
 package lecture3;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.TreeSet;
+
+/*
+Время для 10 тысяч вставок
+*/
+
 public class Solution1 {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        LinkedList list1 = new LinkedList();
+
+        System.out.println("Время добавления 1000 элементов в ArrayList: " + getInsertTimeInMs(list));
+        System.out.println("Время добавления 1000 элементов в LinkedList " + getInsertTimeInMs(list1));
+        System.out.println("Время поиска 1000 элементов в ArrayList " + getSearchTimeInMs(list));
+        System.out.println("Время поиска 1000 элементов в LinkedList " + getSearchTimeInMs(list1));
+        System.out.println("Время удаления 1000 элементов в ArrayList " + getRemoveTimeInMs(list));
+        System.out.println("Время удаления 1000 элементов в LinkedList " + getRemoveTimeInMs(list1));
+    }
+
+    public static long getInsertTimeInMs(List list) {
+        Date currentDate1 = new Date();
+        insert10000(list);
+        Date currentDate2 = new Date();
+        long msTimeDistance = currentDate2.getTime() - currentDate1.getTime();
+        return msTimeDistance;
+
+    }
+
+    public static long getSearchTimeInMs(List list) {
+        Date currentDate1 = new Date();
+        search10000(list);
+        Date currentDate2 = new Date();
+        long msTimeDistance = currentDate2.getTime() - currentDate1.getTime();
+        return msTimeDistance;
+    }
+
+    public static long getRemoveTimeInMs(List list) {
+        Date currentDate1 = new Date();
+        remove10000(list);
+        Date currentDate2 = new Date();
+        long msTimeDistance = currentDate2.getTime() - currentDate1.getTime();
+        return msTimeDistance;
+    }
+
+    public static void insert10000(List list) {
+        for (int i = 0; i < 10000; i++) {
+            list.add(i);
+        }
+    }
+
+    public static void search10000(List list) {
+        for (int i = 0; i < 10000; i++) {
+            list.get(i);
+        }
+    }
+
+    public static void remove10000(List list) {
+        for (int i = 9999; i >= 0; i--) {
+            list.remove(i);
+        }
+    }
 }
+
+
--- src/main/java/lecture3/Solution4.java
+++ src/main/java/lecture3/Solution4.java
@@ -1,4 +1,72 @@
 package lecture3;
 
+import java.util.Date;
+import java.util.TreeSet;
+import java.util.HashSet;
+
+
 public class Solution4 {
+    public static void main(String[] args) {
+        TreeSet<Integer> set = new TreeSet<Integer>();
+        HashSet<Integer> set1 = new HashSet<Integer>();
+
+        // Добавление 10000 элементов в TreeSet
+        Date currentDate = new Date();
+        for (int i = 0; i < 10000; i++) {
+            set.add(i);
+        }
+        Date currentDate1 = new Date();
+        long msTimeDistance = currentDate1.getTime() - currentDate.getTime();
+        System.out.println("Время добавления 10000 элементов в TreeSet: " + msTimeDistance);
+
+        // Добавление 10000 элементов в HashSet
+        Date currentDate2 = new Date();
+        for (int i = 0; i < 10000; i++) {
+            set1.add(i);
+        }
+        Date currentDate3 = new Date();
+        long msTimeDistance1 = currentDate3.getTime() - currentDate2.getTime();
+        System.out.println("Время добавления 10000 элементов в HashSet: " + msTimeDistance1);
+
+        // Поиск 10000 в TreeSet
+        Date currentDate4 = new Date();
+        for (Integer s: set) {
+            set.contains(s);
+            Date currentDate5 = new Date();
+            long msTimeDistance2 = currentDate5.getTime() - currentDate4.getTime();
+            System.out.println("Время поиска 10000 элементов в TreeSet: " + msTimeDistance2);
+
+
+            //Поиск 10000 в HashSet
+            Date currentDate6 = new Date();
+            for (Integer l: set1) {
+                set1.contains(l);
+            }
+            Date currentDate7 = new Date();
+            long msDistance3 = currentDate7.getTime() - currentDate6.getTime();
+            System.out.println("Время поиска 10000 в HashSet: " + msDistance3);
+
+
+            //Удаление 10000 элементов в TreeSet
+            Date currentDate8 = new Date();
+            for (int i = 9999; i>= 0; i--) {
+                set.remove(i);
+            }
+            Date currentDate9 = new Date();
+            long msDistance4 = currentDate9.getTime() - currentDate8.getTime();
+            System.out.println("Время удаления 10000 элементов в HashSet: " + msDistance4);
+
+            //Удаление 10000 элементов в HashSet
+            Date currentDate10 = new Date();
+            for (int i = 9999; i >=0; i--) {
+                set1.remove(i);
+            }
+            Date currentDate11 = new Date();
+            long msDistance5 = currentDate11.getTime() - currentDate10.getTime();
+            System.out.println("Время удаления 10000 в HashSet: " + msDistance5);
+        }
+
+    }
+
 }
+
